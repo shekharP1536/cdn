39b8136ea4873d374ec5f76e8fab1f4730c2b815
Shekhar Pachlore <83689949+shekharP1536@users.noreply.github.com>
2024-11-09 14:24:36 +0530

    botornot

---
+++ custom.js
@@ -0,0 +1,102 @@
+let questions = [
+  {
+      "numb": 1,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013240.png"
+  },
+  {
+      "numb": 2,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013058.png"
+  },
+  {
+      "numb": 3,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013333.png"
+  },
+  {
+      "numb": 4,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013413.png"
+  },
+  {
+      "numb": 5,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013447.png"
+  },
+  {
+      "numb": 6,
+      "question": "Which one is AI generated?",
+      "answer": "A",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013555.png"
+  },
+  {
+      "numb": 7,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013743.png"
+  },
+  {
+      "numb": 8,
+      "question": "Which one is a real photo?",
+      "answer": "B",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013844.png"
+  },
+  {
+      "numb": 9,
+      "question": "Which one is AI generated?",
+      "answer": "A",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20013932.png"
+  },
+  {
+      "numb": 10,
+      "question": "Which one is AI generated?",
+      "answer": "A",
+      "options": [
+          "A",
+          "B"
+      ],
+      "image": "https://cdn.jsdelivr.net/gh/SoumojitBhuin/cdn@main/Screenshot%202024-11-09%20014115.png"
+  }
+]
